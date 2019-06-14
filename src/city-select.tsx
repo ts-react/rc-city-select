deaec0ff6a20467418317888eaa823186aa2305e
wangxingkang <wangxingkang@sensoro.com>
2019-06-14 12:36:46 +0800

    chore: 搭建开发环境

---
+++ src/city-select.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export interface ICitySelectProps {
+  className?: string;
+}
+
+class CitySelect extends React.Component<ICitySelectProps, any> {
+  render() {
+    return (
+      <div>
+        CitySelect
+      </div>
+    )
+  }
+}
+
+export default CitySelect;
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import CitySelect from './city-select';
+
+export { ICitySelectProps } from './city-select';
+export default CitySelect;
