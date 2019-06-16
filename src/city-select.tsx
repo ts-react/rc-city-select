3bbaf63f0d28a786457dc6f45926bca15e23d521
wangxingkang <wangxingkang@sensoro.com>
2019-06-16 22:28:06 +0800

    feat: 功能基本实现

--- src/city-select.tsx
+++ src/city-select.tsx
@@ -1,15 +1,73 @@
 import React from 'react';
+import classNames from 'classnames';
+import { Cascader } from 'antd';
+import { CascaderProps, FieldNamesType } from 'antd/es/cascader';
+import cityData from '@jiumao/china-city-data';
+import './city-select.less';
 
-export interface ICitySelectProps {
-  className?: string;
+export interface ICitySelectProps extends CascaderProps {
+  // 选择的值的类型
+  valueType: 'name' | 'code';
 }
 
-class CitySelect extends React.Component<ICitySelectProps, any> {
+interface IState {
+  fieldNames: FieldNamesType;
+}
+
+const prefixCls = 'rc-city-select';
+
+class CitySelect extends React.Component<ICitySelectProps, IState> {
+  static defaultProps = {
+    changeOnSelect: true,
+    valueType: 'name'
+  };
+  readonly state: IState = {
+    fieldNames: {
+      label: 'n',
+      value: 'n',
+      children: 'c'
+    }
+  };
+
+  componentDidMount() {
+    const { valueType } = this.props;
+
+    if (valueType === 'name') {
+      this.setState({
+        fieldNames: {
+          label: 'n',
+          value: 'n',
+          children: 'c'
+        }
+      })
+    } else {
+      this.setState({
+        fieldNames: {
+          label: 'n',
+          value: 'v',
+          children: 'c'
+        }
+      })
+    }
+  }
+
   render() {
+    const { className, valueType, ...rest } = this.props;
+    const { fieldNames } = this.state;
+
     return (
-      <div>
-        CitySelect
-      </div>
+      <Cascader
+        className={classNames(className, {
+          [`${prefixCls}`]: true
+        })}
+        options={cityData}
+        onChange={(value, selectedOptions) => {
+          console.log(value);
+          console.log(selectedOptions);
+        }}
+        {...rest}
+        fieldNames={fieldNames}
+      />
     )
   }
 }
